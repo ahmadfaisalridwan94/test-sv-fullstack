0b536465355e801c295c7e3cfbf04116147646b4
Ahmad Faisal Ridwan <ahmadfaisal.ridwan@gmail.com>
2023-06-07 17:05:54 +0700

    Update router. delete unused import

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import DefaultLayout from "../components/DefaultLayout.vue";
 import Home from "../views/Home.vue";
-import Post from "../views/Post.vue";
 import CreatePost from "../views/CreatePost.vue";
 import EditPost from "../views/EditPost.vue";
 import Preview from "../views/Preview.vue";
@@ -15,12 +14,7 @@ const routes = [
                 path: '/',
                 name: 'home',
                 component: Home,
-            },
-            {
-                path: '/posts',
-                name: 'post',
-                component: Post,
-            },
+            },           
             {
                 path: '/post/edit/:id',
                 name: 'edit_post',
